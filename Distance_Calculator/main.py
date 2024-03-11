2ec7ca98a512a559c7e2d9bebcde02d35da2fb91
HectorTheJunior <160748931+HectorTheJunior@users.noreply.github.com>
2024-03-11 15:41:05 +0100

    Create main.py

---
+++ Distance_Calculator/main.py
@@ -0,0 +1,49 @@
+from dataclasses import dataclass
+from geopy.geocoders import Nominatim
+from geopy.distance import geodesic
+
+
+@dataclass
+class Coordinates:
+    latitude: float
+    longitude: float
+
+    def coordinates(self):
+        return self.latitude, self.longitude
+
+
+def calculate_distance_km(home, target):
+    if home and target:
+        distance = geodesic(home.coordinates(), target.coordinates()).km
+    return distance
+
+
+def get_distance_km(home, target):
+    home_coors = get_coordinates(home)
+    target_coors = get_coordinates(target)
+
+    if distance := calculate_distance_km(home_coors, target_coors):
+        print(f'{home} --> {target}')
+        print(f'{distance:.2f} km')
+        return distance
+    else:
+        print("Failed to calculate the distance")
+
+
+def get_coordinates(address) -> Coordinates|None:
+    geolocator = Nominatim(user_agent='distance_calculator')
+
+    location = geolocator.geocode(address)
+
+    if location:
+        return Coordinates(latitude=location.latitude, longitude=location.longitude)
+
+
+def main():
+    home_address = input('Enter first address: ')
+    target_address = input('Enter second address: ')
+    print("Calculating.....")
+    get_distance_km(home=home_address, target=target_address)
+
+
+main()
